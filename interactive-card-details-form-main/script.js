1784213bded88ee7693ea66144e49ca9918c9418
Joeybervin <61996287+Joeybervin@users.noreply.github.com>
2023-08-16 23:44:28 +0200

    mise en place front FIN

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -1,16 +1,55 @@
-let creditCardNumberDOM = document.querySelector(".cardInformations__container > p")
+let cardNumberDOM = document.querySelector(".cardInformations__container > p")
+let formValid = false
 
 
 
-
-let styleCreditCardNumber = (creditCardNumber) => {
-    let numberSplited = creditCardNumber.match(/.{1,4}/g) ?? [];
+let cardNumberStyle = (cardNumber) => {
+    let numberSplited = cardNumber.match(/.{1,4}/g) ?? [];
 
     for (let i = 0; i < numberSplited.length; i++) {
         let span = document.createElement('span');
         span.innerHTML = numberSplited[i];
-        creditCardNumberDOM.appendChild(span);
+        cardNumberDOM.appendChild(span);
     }
 }
 
-styleCreditCardNumber("0000000000000000")
+const inputEmpty = (inputDOM) => {
+    if (inputDOM === "" || inputDOM === " ") {
+        document.querySelector("")
+    }
+}
+
+const emptyInputCheck = (inputValue, inputErrorMessageDOM) => {
+    if ( inputValue === "" ) inputErrorMessageDOM.innerHTML = "Can't be blanck";
+}
+
+const letterInputCheck = (inputValue, errorTargetId) => {
+    let hasLetter = /[a-zA-Z]/.test(inputValue);
+    const errorTarget = document.getElementById(errorTargetId);
+    if (hasLetter) {
+        errorTarget.innerHTML = "Wrong format, numbers only" }
+    else {
+        errorTarget.innerHTML = "" }
+}
+
+const form = document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    /* Inputs values*/
+    let name = document.getElementById("name").value;
+    let cardNumber = document.getElementById("cardNumber").value;
+    let dateMonth = document.getElementById("dateMonth").value;
+    let dateYear = document.getElementById("dateYear").value;
+    let cvc = document.getElementById("cvc").value;
+
+    let formInputValueArray = [name, cardNumber, dateMonth, dateYear, cvc]
+
+
+
+
+
+    
+    console.log(coco)
+})
+
+cardNumberStyle("0000000000000000")
