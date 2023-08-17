ffb1d7df5b12cbe17f204883953910ac3ab1f52d
Joeybervin <61996287+Joeybervin@users.noreply.github.com>
2023-08-17 19:40:49 +0200

    END
    Implementation of the latest features on the page
    v2: Adding some animations

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -1,55 +1,162 @@
 let cardNumberDOM = document.querySelector(".cardInformations__container > p")
-let formValid = false
+let formValid = [false, false, false, false, false];
 
-
-
-let cardNumberStyle = (cardNumber) => {
-    let numberSplited = cardNumber.match(/.{1,4}/g) ?? [];
-
+const applyCardNumberStyle = (cardNumber, targetElement) => {
+    targetElement.innerHTML = "";
+    let numbercleaned = cardNumber.replace(/\s/g, "")
+    let numberSplited = numbercleaned.match(/.{1,4}/g) ?? [];
     for (let i = 0; i < numberSplited.length; i++) {
         let span = document.createElement('span');
         span.innerHTML = numberSplited[i];
-        cardNumberDOM.appendChild(span);
+        targetElement.appendChild(span);
     }
 }
 
-const inputEmpty = (inputDOM) => {
-    if (inputDOM === "" || inputDOM === " ") {
-        document.querySelector("")
+const applyRedBorder = (inputElement, state) => {
+    if (state === "error") inputElement.classList.add("error");
+    else inputElement.classList.remove("error");
+}
+
+const preventSpaceInput = (inputElement) => {
+    inputElement.addEventListener("keydown", function(event) {
+        if (event.key === " ") {
+            event.preventDefault();
+            applyRedBorder(inputElement, "error")
+        }
+        if (inputElement.value.length === 1 && inputElement.value[0] === " ") {
+            this.value = ""; // Effacer l'espace au début
+        }
+    });
+};
+
+const validateInput = (inputElement,inputValue, errorMessage, index) => {
+
+    formValid[index] = false;
+    if (inputValue.trim() === "") {
+        errorMessage.innerHTML = "Can't be blank";
+        applyRedBorder(inputElement, "error")
+    } else if (/[a-zA-Z]/.test(inputValue)) {
+        errorMessage.innerHTML = "Wrong format, numbers only";
+        applyRedBorder(inputElement, "error")
+    } else {
+        errorMessage.innerHTML = "";
+        applyRedBorder(inputElement, "no-error")
+        formValid[index] = true;
     }
-}
+};
 
-const emptyInputCheck = (inputValue, inputErrorMessageDOM) => {
-    if ( inputValue === "" ) inputErrorMessageDOM.innerHTML = "Can't be blanck";
-}
+/* Add a 0 at the beginning */
+const addZeroAtBeginning = document.getElementById("dateMonth").addEventListener("blur", function() {
+    const inputValue = this.value.trim();
+    
+    if (inputValue.length === 1 && /^[1-9]$/.test(inputValue)) {
+        this.value = "0" + inputValue;
+    }
+});
 
-const letterInputCheck = (inputValue, errorTargetId) => {
-    let hasLetter = /[a-zA-Z]/.test(inputValue);
-    const errorTarget = document.getElementById(errorTargetId);
-    if (hasLetter) {
-        errorTarget.innerHTML = "Wrong format, numbers only" }
+/* cardHOLDER NAME */
+document.getElementById("name").addEventListener("input", function() {
+
+    formValid[0] = false;
+    if (this.value.trim() === "") { 
+        document.querySelector("span.errorMessage__text--name").innerHTML = "Can't be blank";
+        applyRedBorder(this, "error");
+    } else if (/\d/.test(this.value)) { 
+        document.querySelector("span.errorMessage__text--name").innerHTML = "Wrong format, letters only";
+        applyRedBorder(this, "error");
+    } else {
+        applyRedBorder(this, "no-error");
+        document.querySelector("span.errorMessage__text--name").innerHTML = "";
+        formValid[0] = true;
+    }
+});
+/* CARD NUMBER */
+document.getElementById("cardNumber").addEventListener("input", function() {
+    preventSpaceInput(this);
+    let inputValue = this.value.replace(/\s/g, ''); // Supprimer les espaces existants
+    let formattedValue = inputValue.match(/.{1,4}/g)?.join(' ') || ''; // Ajouter des espaces après chaque groupe de 4 caractères
+    this.value = formattedValue;
+
+    if (this.value.length < 16) formValid[1] = false;
+    validateInput(this, this.value, document.querySelector("span.errorMessage__text--cardNumber"),1);
+});
+/* DATE MONTH + YEAR */
+document.getElementById("dateMonth").addEventListener("input", function() {
+    preventSpaceInput(this);
+    if (this.value <= 0 || this.value > 12 ) {
+        document.querySelector("span.errorMessage__text--expDate").innerHTML = "Invalid";
+        formValid[2] = false;
+        applyRedBorder(this, "error")
+    }else {
+        validateInput(this, this.value, document.querySelector("span.errorMessage__text--expDate"),2) }
+});
+document.getElementById("dateYear").addEventListener("input", function() {
+    preventSpaceInput(this);
+    if (this.value < 23) {
+        document.querySelector("span.errorMessage__text--expDate").innerHTML = "Invalid";
+        applyRedBorder(this, "error")
+        formValid[3] = false
+    } else {
+        validateInput(this, this.value, document.querySelector("span.errorMessage__text--expDate"),3);
+    }
+});
+/* CVC */
+document.getElementById("cvc").addEventListener("input", function() {
+    preventSpaceInput(this);
+    if (this.value.length < 3) {
+        document.querySelector("span.errorMessage__text--cvc").innerHTML = "Invalid";
+        applyRedBorder(this, "error");
+        formValid[4] = false;
+    } else {
+        validateInput(this, this.value, document.querySelector("span.errorMessage__text--cvc"),4); }
+});
+
+const resetForm = () => {
+    document.querySelector("form").reset();
+};
+
+const showAndHideForm = (action) => {
+    if (action === "hide") {
+        document.querySelector(".formSubmitAnswer").classList.remove("hide")
+        document.querySelector("form").classList.add("hide")
+    }
     else {
-        errorTarget.innerHTML = "" }
+        document.querySelector("form").classList.remove("hide")
+        document.querySelector(".formSubmitAnswer").classList.add("hide")
+    }
 }
 
-const form = document.querySelector("form").addEventListener("submit", (e) => {
+const submitForm = document.querySelector("form").addEventListener("submit", (e) => {
+
     e.preventDefault();
 
-    /* Inputs values*/
-    let name = document.getElementById("name").value;
-    let cardNumber = document.getElementById("cardNumber").value;
-    let dateMonth = document.getElementById("dateMonth").value;
-    let dateYear = document.getElementById("dateYear").value;
-    let cvc = document.getElementById("cvc").value;
-
-    let formInputValueArray = [name, cardNumber, dateMonth, dateYear, cvc]
-
-
-
+    if (!formValid.includes(false)) { 
+        /* Inputs values*/
+        document.querySelector(".cardInformations__container > div > p").innerHTML = document.getElementById("name").value;
+        applyCardNumberStyle(document.getElementById("cardNumber").value, cardNumberDOM );
+        document.querySelector(".cardInformations__container > div > p span ").innerHTML = document.getElementById("dateMonth").value;
+        document.querySelector(".cardInformations__container > div > p span + span").innerHTML = document.getElementById("dateYear").value;
+        document.querySelector(".card__container--back > div > p").innerHTML = document.getElementById("cvc").value
+        showAndHideForm("hide")
+    }
 
 
-    
-    console.log(coco)
+
+    console.log(formValid)
 })
 
-cardNumberStyle("0000000000000000")
+const cleanOldValues = document.querySelector("button#continue").addEventListener("click", () => {
+
+    formValid = [false, false, false, false, false];
+    /* DOM cards old values */
+    document.querySelector(".cardInformations__container > div > p").innerHTML = "Jane Appleseed";
+    applyCardNumberStyle("0000000000000000", cardNumberDOM);
+    document.querySelector(".card__container--back > div > p").innerHTML = "000";
+    document.querySelector(".cardInformations__container > div > p span ").innerHTML = "00"
+    document.querySelector(".cardInformations__container > div > p span + span ").innerHTML = "00"
+
+    resetForm();
+    showAndHideForm("show")
+})
+
+applyCardNumberStyle("0000000000000000", cardNumberDOM)
